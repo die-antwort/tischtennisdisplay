4e4eee7c262afb449791abacd7777da7dcf7c955
Stefan Daschek <stefan@die-antwort.eu>
2017-04-17 16:45:31 +0000

    Use semaphores in ButtonInput; command line argument for "pi" mode; cleanup

--- button_input.rb
+++ button_input.rb
@@ -1,44 +1,45 @@
 require "pi_piper"
+require "concurrent/atomic/semaphore"
+
 class ButtonInput
   DOUBLE_CLICK_DELAY = 0.2
   DEBOUNCE_DELAY = 0.1
 
-  LEFT_PIN = 1
-  RIGHT_PIN = 2
-
-  def initialize
+  def initialize(left_button_pin, right_button_pin)
     @inputs = []
-    connect(LEFT_PIN, ->  (action) { handle_symbol(action, 'l') })
-    connect(RIGHT_PIN, ->  (action) { handle_symbol(action, 'r') })
+    @semaphore = Concurrent::Semaphore.new(0)
+    connect(left_button_pin, ->(action) { handle_symbol(action, 'l') })
+    connect(right_button_pin, ->(action) { handle_symbol(action, 'r') })
   end
 
-  def handle_symbol(action, side) 
+  def get_next
+    @semaphore.acquire if @inputs.empty?
+    @inputs.shift
+  end
+
+  private
+
+  def handle_symbol(action, side)
     if action == :click
       @inputs.push(side)
     else
       @inputs.push('u')
     end
+    @semaphore.release
   end
 
-  def get_next
-    loop do 
-      while @input.empty?; end
-      @input.shift
-    end
-  end
-
-  def connect(button_pin_nr, ƛ)
+  def connect(button_pin_nr, handler)
     t = nil
     PiPiper.after pin: button_pin_nr, goes: :down, pull: :up do
       sleep(DEBOUNCE_DELAY) # debounce button (min click time-'distance')
       if t.nil? || !t.alive?
         t = Thread.new{
           sleep DOUBLE_CLICK_DELAY
-          ƛ.call(:click)
+          handler.call(:click)
         }
       else
         t.kill
-        ƛ.call(:double_click)
+        handler.call(:double_click)
       end
     end
   end
--- main.rb
+++ main.rb
@@ -1,27 +1,19 @@
 #!/usr/bin/env ruby
 require "bundler"
+require_relative "button_input"
+require_relative "console_input"
+require_relative "console_score_board"
+require_relative "game"
+
 
 PINS = {
-  p1_button_pin: 3,
-  p2_button_pin: 2,
+  left_button_pin: 3,
+  right_button_pin: 2,
   clock_pin: 17,
 }.freeze
 
 P1_SHIFT_REGISTER = '/dev/spidev0.0'.freeze
 P2_SHIFT_REGISTER = '/dev/spidev0.1'.freeze
-
-
-PINS.values.each do |pin|
-  # system "echo #{pin} > /sys/class/gpio/unexport 2>/dev/null";
-end
-
-# require "pi_piper"
-# include PiPiper
-Thread.abort_on_exception = true
-
-require_relative "game"
-require_relative "console_score_board"
-require_relative "console_input"
 
 def update_score_board(score)
   options =
@@ -37,14 +29,24 @@ def update_score_board(score)
   @score_board.display(score.for_side(:left), score.for_side(:right), **options)
 end
 
-@input = ConsoleInput.new
-# @input = ButtonInput.new
 
-@score_board = ConsoleScoreBoard.new
+Thread.abort_on_exception = true
+
+if ARGV.shift == "pi"
+  PINS.values.each do |pin|
+    system "echo #{pin} > /sys/class/gpio/unexport 2>/dev/null";
+  end
+  @input = ButtonInput.new(PINS[:left_button_pin], PINS[:right_button_pin])
+  @score_board = ConsoleScoreBoard.new # FIXME
+else
+  @input = ConsoleInput.new
+  @score_board = ConsoleScoreBoard.new
+end
+
 @score_board.display("3", "5", blink: :both)
 max_set_count = @input.get_next == 'l' ? 3 : 5
+puts "Starting a best of #{max_set_count} game."
 game = Game.new(max_set_count: max_set_count)
-
 
 loop do
   c = @input.get_next
@@ -57,12 +59,3 @@ loop do
   break if game.game_finished?
 end
 
-
-
-
-
-
-
-# Game.new(**PINS, p1_shift_register: P1_SHIFT_REGISTER, p2_shift_register: P2_SHIFT_REGISTER)
-
-# wait
