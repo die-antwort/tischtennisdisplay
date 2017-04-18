141985e9061762c6846cc442b67e083648a24d62
Stefan Daschek <stefan@die-antwort.eu>
2017-04-18 11:41:14 +0000

    Use blocks instead of lambdas for callbacks.
    That's a bit more idiomatic Ruby. And a bit shorter, too.

--- button_input.rb
+++ button_input.rb
@@ -7,8 +7,8 @@ class ButtonInput
 
   def initialize(left_button_pin, right_button_pin)
     @inputs = Concurrent::Channel.new(capacity: 1000)
-    connect(left_button_pin, ->(action) { handle_symbol(action, 'l') })
-    connect(right_button_pin, ->(action) { handle_symbol(action, 'r') })
+    connect(left_button_pin){ |action| handle_symbol(action, 'l') }
+    connect(right_button_pin){ |action| handle_symbol(action, 'r') }
   end
 
   def get_next
@@ -25,18 +25,18 @@ class ButtonInput
     end
   end
 
-  def connect(button_pin_nr, handler)
+  def connect(button_pin_nr)
     t = nil
     PiPiper.after pin: button_pin_nr, goes: :down, pull: :up do
       sleep(DEBOUNCE_DELAY) # debounce button (min click time-'distance')
       if t.nil? || !t.alive?
         t = Thread.new{
           sleep DOUBLE_CLICK_DELAY
-          handler.call(:click)
+          yield :click
         }
       else
         t.kill
-        handler.call(:double_click)
+        yield :double_click
       end
     end
   end
