27d945ba02b070d9fb1868349ea1b07edfe21794
Stefan <stefan@die-antwort.eu>
2019-10-06 23:01:46 +0200

    Score board can now display longer strings via scrolling
    Only some characters are currently defined, though.

--- score_board.rb
+++ score_board.rb
@@ -1,16 +1,17 @@
-require_relative "./integer_to_score_board_converter"
+require_relative "./string_to_score_board_converter"
 require_relative "./untroubled_pi_piper"
 
 class ScoreBoard
   BLINK_DELAY = 0.500
   FLASH_TWICE_DELAY = 0.250
   ROTATE_DELAY = 0.100
+  SCROLL_DELAY = 0.500
 
   def initialize(p1_shift_register, p2_shift_register, clock_pin)
     @p1_shift_register = p1_shift_register
     @p2_shift_register = p2_shift_register
-    @left_bits = 0
-    @right_bits = 0
+    @left_str = nil
+    @right_str = nil
     @effect = nil
     @side = nil
     init_clock_pin(clock_pin)
@@ -22,41 +23,47 @@ class ScoreBoard
       bit_sequence = nil
       flash_twice_count = 0
       loop do
+        left_bits = @left_str ? StringToScoreBoardBitConverter.convert(@effect == :scroll ? @left_str.next : @left_str) : nil
+        right_bits = @right_str ? StringToScoreBoardBitConverter.convert(@effect == :scroll ? @right_str.next : @right_str) : nil
+
         case @effect
         when :blink
-          update(@left_bits, @right_bits)
+          update(left_bits, right_bits)
           sleep(BLINK_DELAY)
-          update((@left_bits if @side == :right), (@right_bits if @side == :left))
+          update((left_bits if @side == :right), (right_bits if @side == :left))
           sleep(BLINK_DELAY)
         when :blink_alternating
-          update(@left_bits, nil)
+          update(left_bits, nil)
           sleep(BLINK_DELAY)
-          update(nil, @right_bits)
+          update(nil, right_bits)
           sleep(BLINK_DELAY)
         when :flash_twice
-          update(@left_bits, @right_bits)
+          update(left_bits, right_bits)
           sleep(FLASH_TWICE_DELAY)
-          update((@left_bits if @side == :right), (@right_bits if @side == :left))
+          update((left_bits if @side == :right), (right_bits if @side == :left))
           sleep(FLASH_TWICE_DELAY)
           if (flash_twice_count += 1) == 2
             flash_twice_count = 0
             @effect = nil
           end
         when :rotate_cw
-          bit_sequence = IntegerToScoreBoardBitConverter.rotation_sequence_cw.cycle
+          bit_sequence = StringToScoreBoardBitConverter.rotation_sequence_cw.cycle
           @effect = :rotate_continue
         when :rotate_ccw
-          bit_sequence = IntegerToScoreBoardBitConverter.rotation_sequence_ccw.cycle
+          bit_sequence = StringToScoreBoardBitConverter.rotation_sequence_ccw.cycle
           @effect = :rotate_continue
         when :rotate_bounce
-          bit_sequence = IntegerToScoreBoardBitConverter.rotation_sequence_bounce.cycle
+          bit_sequence = StringToScoreBoardBitConverter.rotation_sequence_bounce.cycle
           @effect = :rotate_continue
         when :rotate_continue
           rotation_bits = bit_sequence.next
-          update(@side != :right ? rotation_bits : @left_bits, @side != :left ? rotation_bits : @right_bits)
+          update(@side != :right ? rotation_bits : left_bits, @side != :left ? rotation_bits : right_bits)
           sleep(ROTATE_DELAY)
+        when :scroll
+          update(left_bits, right_bits)
+          sleep(SCROLL_DELAY)
         else
-          update(@left_bits, @right_bits)
+          update(left_bits, right_bits)
         end
       end
     }
@@ -64,11 +71,13 @@ class ScoreBoard
 
   #just updating state here
   #thread @t redraws it
-  def display(left_score, right_score, effect: nil, side: nil)
-    @left_bits = left_score ? IntegerToScoreBoardBitConverter.convert(left_score) : nil
-    @right_bits = right_score ? IntegerToScoreBoardBitConverter.convert(right_score) : nil
+  def display(left, right, effect: nil, side: nil)
+    @left_str = left.to_s
+    @right_str = right.to_s
     @effect = effect
     @side = side
+    @left_str = @left_str.split(//).cycle if @left_str && effect == :scroll && side != :right
+    @right_str = @right_str.split(//).cycle if @right_str && effect == :scroll && side != :left
   end
 
   def init_clock_pin(clock_pin)
---
+++ string_to_score_board_converter.rb
@@ -0,0 +1,62 @@
+class StringToScoreBoardBitConverter
+  # The bits map to the segments as follows (0 = LSB, 7 = MSB)
+  #
+  #                1
+  #              * * *
+  #     *      *       *
+  #   0 *    6 *       * 2
+  #     *      *   7   *
+  #              * * *
+  #     *      *       *
+  #   0 *    5 *       * 3
+  #     *      *       *
+  #              * * *
+  #                4
+
+  CHARACTERS = {
+    " " => 0b0000_0000,
+    "0" => 0b0111_1110,
+    "1" => 0b0000_1100,
+    "2" => 0b1011_0110,
+    "3" => 0b1001_1110,
+    "4" => 0b1100_1100,
+    "5" => 0b1101_1010,
+    "6" => 0b1111_1010,
+    "7" => 0b0000_1110,
+    "8" => 0b1111_1110,
+    "9" => 0b1101_1110,
+    "C" => 0b0111_0010,
+    "E" => 0b1111_0010,
+    "I" => 0b0110_0000,
+    "R" => 0b1010_0000,
+    "S" => 0b1101_1010,
+    "V" => 0b0011_1000,
+  }.freeze
+
+  def self.convert(string_or_int)
+    string = string_or_int.to_s
+    raise ArgumentError, "Can not convert '#{string_or_int}'." unless string =~ /\A1?[#{CHARACTERS.keys}]\z/
+
+    if string.length == 2
+      CHARACTERS.fetch(string[1]) | 0b0000_0001
+    else
+      CHARACTERS.fetch(string[0])
+    end
+  end
+
+  def self.rotation_sequence_cw
+    [0b0000_0010, 0b0000_0100, 0b0000_1000, 0b0001_0000, 0b0010_0000, 0b0100_0000]
+  end
+
+  def self.rotation_sequence_ccw
+    rotation_sequence_cw.reverse
+  end
+
+  def self.rotation_sequence_bounce
+    bit_sequence = rotation_sequence_cw * 4
+    bit_sequence.pop
+    bit_sequence += rotation_sequence_ccw * 4
+    bit_sequence.pop
+    bit_sequence
+  end
+end
